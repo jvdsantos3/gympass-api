fb7e24346795a636b47376b964074980b702f614
João <jv.dossantos012@gmail.com>
2023-07-16 15:22:21 -0300

    Implementação dos controllers de check-ins e gyms

--- src/use-cases/factories/make-check-in-use-case.ts
+++ src/use-cases/factories/make-check-in-use-case.ts
@@ -2,7 +2,7 @@ import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-reposito
 import { CheckInUseCase } from '../check-in'
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
 
-export function makeCheckInsUseCase() {
+export function makeCheckInUseCase() {
   const checkInsRepository = new PrismaCheckInsRepository()
   const gymsRepository = new PrismaGymsRepository()
 
